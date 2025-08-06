d21e6f302f576f3612946e597f48118dccca285f
achonline4 <achonline4@gmail.com>
2025-08-06 20:32:42 +0530

    Complete ownership chapter

--- Rust/projects/rust-abridged/ownership-references-and-slices-4/src/main.rs
+++ Rust/projects/rust-abridged/ownership-references-and-slices-4/src/main.rs
@@ -5,11 +5,23 @@ fn main() {
 
     let new_heap_mem = deep_copy_data(outer_string);
     println!("new string content: {}", new_heap_mem);
+
+    println!("string slice 1-4 {} from string {}", slice(&new_heap_mem, 1, 4), new_heap_mem);
+}
+
+/* Return a slice of string between given index */
+fn slice(s: &String, start: usize, end: usize) -> &str {
+    use std::ops::Range;
+    let r: Range<usize> = start..end;
+    /* Both of the following formats are OK */
+    //let slice: &str = &s[1..4];
+    let slice = &s[r];
+    slice
 }
 
 fn deep_copy_data(s: String) -> String {
     println!("Input for deep copy: {}", s);
-    let newstr = s.clone();
+    let newstr = s.clone(); // create a new instance on the heap and copy all data to it.
     return newstr;
 }
 
