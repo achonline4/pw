007d81bf59293d5454ccfb2a6dff9cb055cd330f
achonline4 <achonline4@gmail.com>
2025-08-14 10:55:30 +0530

    Update structs

--- Rust/projects/rust-abridged/structs-5/src/main.rs
+++ Rust/projects/rust-abridged/structs-5/src/main.rs
@@ -1,3 +1,20 @@
+/* Individual elements are not mutable
+ * Either the whole struct is mutable or none of it. */
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    // NOTE: that we are not declaring variable names with 'let' here. We are just
+    // specifying the struct field names, that is required when we declare variable names.
+} // NOTE: we don't need a terminating ';' like in C.
 fn main() {
     println!("Using structs");
+    let mut user_ac = User {
+        active: true,
+        username: String::from("ac"),
+        email: String::from("ac@gmail.com"),
+        sign_in_count: 1,
+    };  /* here we use a semi colon to terminate the struct definition */
+    println!("struct {}", user_ac.username);
 }
